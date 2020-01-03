8502063b408d03158184f6c15ff9ba9b92e910d4
joeyko <joeyko1004@gmail.com>
2020-01-04 00:18:03 +0800

    fix: username and pwd

--- docker/mongo/setup.sh
+++ docker/mongo/setup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
 mongo <<EOF
-
 use admin;
-
 db.auth('root', '123456');
-
 use dmx_aluminum;
-
-db.createUser({user:'test',pwd:'test',roles:[{role:'readWrite',db:'test'}]});
-
+db.createUser({user:'test',pwd:'test',roles:[{role:'readWrite',db:'zhihu'}]});
 db.createCollection("user");
-
 EOF
